42fa1780f8c957d2913d14c8f6350bd4f6ff1520
ardiapriliansyah <ardikun04@gmail.com>
2025-01-20 11:13:23 +0700

    Tim Geocoding

---
+++ layers/DesaIndonesia_0.js
@@ -0,0 +1,8 @@
+var json_DesaIndonesia_0 = {
+"type": "FeatureCollection",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+                                                                                                                                  
+"features": [
+{ "type": "Feature", "properties": { "DESA": "Kampung Blang", "KECAMATAN": "Krueng Sabee", "KABUPATEN": "Aceh Barat", "PROPINSI": "Aceh", "LUAS": 14.439200, "MI_PRINX": "2311", "poto": "E:\/Gis destop\/kelompok\/tugas 3 akhir\/aceh\/poto\/kampung blang.htm" }, "geometry": { "type": "Polygon", "coordinates": [ [ [ 95.559152, 4.681843 ], [ 95.571877, 4.688373 ], [ 95.574636, 4.689789 ], [ 95.580705, 4.693334 ], [ 95.594405, 4.689838 ], [ 95.609875, 4.696142 ], [ 95.625959, 4.706829 ], [ 95.636236, 4.714137 ], [ 95.636511, 4.711689 ], [ 95.636528, 4.699987 ], [ 95.629495, 4.687563 ], [ 95.626329, 4.682897 ], [ 95.61665, 4.680441 ], [ 95.594116, 4.675987 ], [ 95.579612, 4.677056 ], [ 95.57282, 4.677557 ], [ 95.568593, 4.678883 ], [ 95.559152, 4.681843 ] ] ] } }
+]
+}
